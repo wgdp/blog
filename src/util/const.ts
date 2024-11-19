023c399d0de17322549df1ea7d8c677bc67fb2c6
wgdp <tatsuyori4649@gmail.com>
2024-11-20 00:10:23 +0900

    Update リンクカード周り

--- src/util/const.ts
+++ src/util/const.ts
@@ -1,2 +1,4 @@
 export const IMAGE_BASE_URL = "https://image.r2.cloudflare.wgdp.dev"
 export const DEFAULT_THUMBNAIL = IMAGE_BASE_URL + "/default-thumbnail.jpg"
+
+export const DESCRIPTION_LIMIT_LENGTH = 130
