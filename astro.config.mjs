38ae0b3ca0dcf446cb5fd81656fb49fd2ee589de
wgdp <whigdrap@gmail.com>
2025-06-16 21:56:41 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,12 @@ import remarkEmbedYouTubePlugin from "./src/plugins/youtube";
 import remarkEmbedLinkCardPlugin from './src/plugins/linkcards';
 import remarkEmbedCaptionImagePlugin from './src/plugins/captionImage';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
+  site: "https://blog.wgdp.dev",
   markdown: {
     remarkRehype: {
       footnoteLabel: ' ',
