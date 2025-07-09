f51a596e9467e832454b24e1676baa7cf33044e6
wgdp <whigdrap@gmail.com>
2025-07-10 03:35:22 +0900

    update all

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import remarkDirective from "remark-directive";
 import remarkEmbedYouTubePlugin from "./src/plugins/youtube";
 import remarkEmbedLinkCardPlugin from './src/plugins/linkcards';
 import remarkEmbedCaptionImagePlugin from './src/plugins/captionImage';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [],
+
   markdown: {
     remarkRehype: {
       footnoteLabel: ' ',
@@ -24,5 +26,9 @@ export default defineConfig({
     shikiConfig: {
       theme: 'rose-pine-dawn'
     },
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
